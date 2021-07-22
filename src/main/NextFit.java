27b9afe9f06729b61a016351a906fa681fa06f98
John <johnlingleeen@1utar.my>
2021-07-22 21:57:32 +0800

    Update NextFit.java

--- src/main/NextFit.java
+++ src/main/NextFit.java
@@ -1,4 +1,4 @@
-package main;
+package NextFit;
 
 import java.util.Scanner;
 
@@ -9,65 +9,69 @@ public class NextFit
 	{
 		Scanner sc = new Scanner(System.in);
 		
-	    System.out.print("Enter the capacity of the bin : ");
+	    System.out.print("Enter the capacity of the bin : "); 
 	    int c = sc.nextInt();
 	    
-	    System.out.print("Enter the numbers of the bin : ");
-	    int m = sc.nextInt();
+	    /*System.out.print("Enter the numbers of the bin : ");
+	    int m = sc.nextInt();*/
 	    
 	    System.out.print("Enter the number of the objects: ");
 	    int numOfobject = sc.nextInt();
 	    
-	    int[] n = new int [numOfobject];
 	    
-	    for (int i =0; i < numOfobject; i++)
+	    int[] n = new int [numOfobject]; // Initialize array n with numOfobject 
+	    
+	    
+	    for (int i =0; i < numOfobject ; i++) 
 	    {
-	    	System.out.print("Object" + (i+1) +" || ");
-		    //int num = GetSize();
-		    
-		    
-		    n [i] = sc.nextInt();
-		    
+	    	System.out.print("Object " + (i+1) +" || "); // Input size of each object
+	    	n [i] = sc.nextInt(); 
+	    	
+	    	if (n[i] > c)	// If size of particular object exceed the capacity of bin
+		    {
+		    	System.out.println("--Error : Size of Object " + (i+1) + " exceed the capacity of the bin !!!--");
+		    	i--;
+		    	
+		    }
+	    	
 	    }
+		
 	    
-	    System.out.println("Number of bins required in Next Fit : " + nextFit(c,n,numOfobject));
+	     System.out.println("Number of bins required in Next Fit : " + nextFit(c,n,numOfobject)); 
+	    
+	    /* for (int i  = 0; i < nextFit(c,n,numOfobject); i++ )
+		{
+	    	
+			System.out.println("Bin" + i+1 + " || " + n[i]);
+		} */
+	   
 
 	}
-	
-	/*public static int GetSize()
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the size of the object : ");
-	    int s = sc.nextInt();
-	    
-	    return s;
-	    
-	}*/
 	
 	public static int nextFit(int c, int n[],int numOfobject )
 	{
-		int currentBin = 0;
-		int remBin = c;
+		int currentBin = 1; //Initialize currentBin required to 1
+		int remBin = c; //Initialize remBin to the capacity of bin
 		
-		for (int i = 0; i < numOfobject; i++)
+		for (int i = 0; i < numOfobject; i++) 
 		{
-			if(n[i] > remBin)
+			if(n[i] > remBin) //If size of object exceed the remBin 
 			{
-				currentBin++;
-				remBin = c - n[i];
+				currentBin++; //Use the new bin 
+				remBin = c - n[i]; //Update remBin by (capacity of current bin - size of object)
+				
 			}
 			else 
 			{
-				remBin -= n[i];
+				remBin -= n[i]; //Update remBin by (remaining capacity of bin - size of object)
 			}
-			
-	
 			
 		}
 		
-		return currentBin;
+		
+		return currentBin; //total number of bins required
 		
 			
 	}
-
+	
 }
